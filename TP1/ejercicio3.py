de32b8a2ee53a6f2a95d89b99d080658624afa3c
[Facundo Merino] <[valid-email]>
2024-03-31 19:34:27 -0300

    TP1 finalizado

--- TP1/ejercicio3.py
+++ TP1/ejercicio3.py
@@ -1,42 +1,37 @@
 import re
 
-
-
-def validad_mail():
-  with open('mail.txt') as archivo:
+def validar_mail():
+  with open('TP1\mail.txt') as archivo:
       mail_data = archivo.readlines()
 
   for line in mail_data:
       patron_mail = '[a-zA-Z0-9-_]+@[a-zA-Z0-9]+.[a-zA-Z]{2,4}(.[a-zA-Z]{2,3})?'
       if re.search(patron_mail, line) is None:
-          return line + " no es un mail."
+          print("{} no es un mail.".format(line.strip()))
       else:
-          return line + " es un mail."
+          print("{} es un mail.".format(line.strip()))
 
-def validad_url(url):
-  patron_url =  '(?:https?:\/\/)?(?:www\.)?[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(\/|\?[a-zA-Z0-9.-=&]+)?'
-  return re.search(patron_url, url)
-
-def validad_ip(ip):
-  patron_ip = '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
-  return re.search(patron_ip, ip)
-
-print(validad_mail())
-
-# for line in text_data:
-#   if re.search(mail, line) is None:
-#     print(line + " no es un mail.")
-#   else:
-#     print(line + " es un mail.")
-
-#   if re.search(url, line) is None:
-#     print(line + " no es un URL.")
-#   else:
-#     print(line + " es un URL.")
-
-#   if re.search(ip, line) is None:
-#     print(line + " no es un IP.")
-#   else:
-#     print(line + " es un IP.")
-
+def validad_url():
+  with open('TP1\\url.txt') as archivo:
+      url_data = archivo.readlines()
     
+  for line in url_data:
+    patron_url = '(?:https?:\/\/)?(?:www\.)?[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(\/|\?[a-zA-Z0-9.-=&]+)?'
+    if re.search(patron_url, line) is None:
+        print("{} no es un URL.".format(line.strip()))
+    else:
+        print("{} es un URL.".format(line.strip()))
+        
+def validad_ip():
+  with open('TP1\IPV4.txt') as archivo:
+      ip_data = archivo.readlines()
+  for line in ip_data:
+    patron_ip = '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
+    if re.search(patron_ip, line) is None:
+        print("{} no es un IP.".format(line.strip()))
+    else:
+        print("{} es un IP.".format(line.strip()))
+
+validar_mail()
+validad_url()
+validad_ip()
