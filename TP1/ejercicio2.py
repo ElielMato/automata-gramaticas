2b120f06c90b2b71b7e705ea90d0c5d96001ee8a
[Facundo Merino] <[valid-email]>
2024-04-20 16:46:45 -0300

    Corrección ejercicio 2 TP1

--- TP1/ejercicio2.py
+++ TP1/ejercicio2.py
@@ -5,15 +5,24 @@ def solve(string):
     regular_expression_operators = '[+*]'
 
     numbers = list(map(int, re.findall(regular_expression_num, string)))
+
     operators = re.findall(regular_expression_operators, string)
+    
+    i = 0
+    while i < len(operators):
+        if operators[i] == '*':
+            numbers[i] *= numbers[i+1]
+            numbers.pop(i+1)
+            operators.pop(i)
+        else:
+            i += 1
 
     result = numbers[0]
     for i in range(1, len(numbers)):
-        if operators[i-1] == "+":
-            result += numbers[i]
-        else:
-            result *= numbers[i]
+        result += numbers[i]
+
     return result
                 
+print(solve("2*2*2+32*2"))   
 
-print(solve("1+1+1*1*1+1"))   
+
